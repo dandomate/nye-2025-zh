392575d5900dc9a082f4b815224883aad261b8ad
dandomate <71004585+dandomate@users.noreply.github.com>
2025-03-16 16:52:08 +0100

    Kész a kód

--- zh/js/index.js
+++ zh/js/index.js
@@ -1,33 +1,20 @@
-/**
- * Complete the functions below, then run `npm run test` in the root directory to check your work.
- * 
- * For more details on expected input and output, check the tests.
- * If all tests are green, you are done! Keep in mind that the actual implementation will be also reviewed.
- * 
- * (don't forget about the html/css task in ../html-css!)
- * 
- * If you find a buggy test, feel free to report (and/or fix) it.
- *
- * ========================================================================================================
- * */
-/**
- * Creates a simple object for HTTP headers based on the input.
- * 
- * The input is in the following format:
- * [
- *  [<Header-Name>, <header-value1>, <header-value2?>, ...],
- *  ...
- * ]
- * 
- * Expected output: {
- *  <header-name>: '<header-value1>, <header-value2>, ...'
- * }
- * 
- */
 module.exports.createHttpHeaders = (input) => {
-    // TODO: your code here
-    return {};
-};
+    const headers = {};
+    if (!Array.isArray(input)) {
+      return headers; 
+    }
+    input.forEach(headerData => {
+      const headerName = headerData[0].toLowerCase();
+      const headerValue = headerData.slice(1).join(', ');
+      if (headers[headerName]) {
+        headers[headerName] += `, ${headerValue}`;
+      } else {
+        headers[headerName] = headerValue;
+      }
+    });
+    return headers;
+  };
+
 
 /**
  * Returns items for a paginated list.
@@ -49,6 +36,17 @@ module.exports.createHttpHeaders = (input) => {
  * ]
  */
 module.exports.getItems = (items, params) => {
-    // TODO: your code here
-    return [];
-}
+    const { page, pageSize, sort } = params;
+    const startIndex = (page - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+  
+    const sortedItems = sort === 'asc'
+      ? [...items].sort((x, y) => x.displayTitle.localeCompare(y.displayTitle))
+      : [...items].sort((x, y) => x.displayTitle.localeCompare(y.displayTitle));
+  
+    return sortedItems.slice(startIndex, endIndex).map(item => ({
+      id: item.id,
+      title: { main: item.displayTitle }
+    }));
+  };
+
